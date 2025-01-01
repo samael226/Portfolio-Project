6ea5c31db4f27120178784ae9c8b5755884c6e2d
idrissmrayah <idrissmrayah@gmail.com>
2025-01-01 02:09:51 +0100

    portfolio

---
+++ portfolio code/portfolio.js
@@ -0,0 +1,5 @@
+function switchiconx(e)
+ { 
+    e = document.querySelector('#img1')
+    e.innerHTML = '<img src="..\portfolio project\cancel_close_delete_exit_logout_remove_x_icon_123217.webp" alt="exist icon "> '
+ }
